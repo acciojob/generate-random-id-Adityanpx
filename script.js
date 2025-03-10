0cbfe7f7cce649697729ad065a7b59fb770b9f47
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-03-10 15:30:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function makeid(l) {
-  // write your code here
+    let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < l; i++) {  // Fixed `:` to `;`
+        result += char.charAt(Math.floor(Math.random() * char.length)); // Fixed `Math`
+    }
+    return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l = parseInt(prompt("Enter a number."), 10); // Convert input to an integer
 alert(makeid(l));
